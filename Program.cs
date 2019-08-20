2068f7fb14496ac2288182120fef461a85e7b8ca
binbwcx <1922935928@qq.com>
2019-08-20 21:17:13 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            Program classa = new Program();
+            classa.mymethod(ref a);
+            Console.WriteLine(a);
+            
+
+        }
+        void mymethod(ref int val)
+        {
+            val += 1;
+
+        }
+    }
+}
